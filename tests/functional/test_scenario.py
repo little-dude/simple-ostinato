651c253915d256fff409e5d1232a760427f23322
Corentin Henry <corentinhenry@gmail.com>
2016-04-26 20:21:33 +0200

    Revert "debug with prints for stuck test"
    This reverts commit 896764dff4c7875d83f4adccff6a71e51505f454.
    
    Tests are not stuck, they just get super slow when creating the
    stream...

--- tests/functional/test_scenario.py
+++ tests/functional/test_scenario.py
@@ -11,17 +11,13 @@ class BaseLayer(object):
     @classmethod
     def setUp(cls):
         # utils.create_ports()
-        print 'a'
         cls._drone_pid = utils.start_drone()
-        print 'b'
         cls.drone = Drone('localhost')
-        print 'c'
         cls.drone.fetch_ports()
         cls.p0 = cls.drone.get_port_by_name('vostinato0')[0]
         cls.p1 = cls.drone.get_port_by_name('vostinato1')[0]
         cls.p0.fetch_streams()
         cls.p1.fetch_streams()
-        print 'd'
 
     @classmethod
     def tearDown(cls):
@@ -40,34 +36,27 @@ class SimpleStreams(unittest.TestCase):
         port_tx = self.layer.p0
         port_rx = self.layer.p1
 
-        print 1
         stream = port_tx.add_stream(
             Mac(source='00:11:22:aa:bb:cc', destination='00:01:02:03:04:05'),
             Ethernet(),
             IPv4(source='10.0.0.1', destination='10.0.0.2'),
             Payload())
 
-        print 2
         stream.name = 'simple_mac_stream'
         stream.packets_per_sec = 1000
         stream.num_packets = 100
         stream.enable()
         stream.save()
 
-        print 3
         port_tx.clear_stats()
         port_rx.clear_stats()
 
-        print 4
         port_rx.start_capture()
         port_tx.start_send()
         time.sleep(1)
-        print 5
         port_tx.stop_send()
         port_rx.stop_capture()
 
-        print 6
         tx_stats = port_tx.get_stats()
         rx_stats = port_rx.get_stats()
         capture = port_rx.save_capture(port_rx.get_capture(), 'capture.pcap')
-        print 7
