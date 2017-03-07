6d5ae1e8c08aec891dbfe8b6b5bb781888266527
Corentin Henry <corentinhenry@gmail.com>
2017-03-07 13:27:53 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='simple_ostinato',
-    version='0.0.3',
+    version='0.0.4',
     include_package_data=True,
     description='simple ostinato python client',
     url='http://github.com/little-dude/simple-ostinato',
